a9861468bf1d33996bfe8bc6b4d9ba70f9eaaf72
Calder Phillips-Grafflin <calder.pg@gmail.com>
2018-03-11 01:39:58 -0500

    Switched to a separate header file for the URScript control program

--- lightweight_ur_interface/src/ur_script_hardware_interface.cpp
+++ lightweight_ur_interface/src/ur_script_hardware_interface.cpp
@@ -23,6 +23,7 @@
 #include <tri_driver_common/math.hpp>
 #include <tri_driver_common/conversions.hpp>
 #include <tri_driver_common/serialization.hpp>
+#include <control_program.hpp>
 
 namespace lightweight_ur_interface
 {
@@ -490,118 +491,13 @@ public:
       const double speed_acceleration,
       const double speed_command_wait)
   {
-    std::string control_script_string = "def ur_driver_program():\n";
-    control_script_string += "  PC_IP_ADDRESS = \"" + our_ip_address + "\"\n";
-    control_script_string += "  PC_CONTROL_PORT = " + std::to_string(control_port) + "\n";
-    control_script_string += "  FLOAT_CONVERSION = " + std::to_string(float_conversion_ratio) + "\n";
-    control_script_string += "  STOP_DECELERATION = " + std::to_string(stop_deceleration) + "\n";
-    control_script_string += "  SPEED_ACCELERATION = " + std::to_string(speed_acceleration) + "\n";
-    control_script_string += "  SPEED_COMMAND_WAIT = " + std::to_string(speed_command_wait) + "\n";
-    control_script_string += "  MODE_IDLE = 0\n";
-    control_script_string += "  MODE_TEACH = 1\n";
-    control_script_string += "  MODE_SPEEDJ = 2\n";
-    control_script_string += "  MODE_SPEEDL = 3\n";
-    control_script_string += "  MODE_WRENCH = 4\n";
-    control_script_string += "  MODE_RIGID = 0\n";
-    control_script_string += "  MODE_FORCE = 1\n";
-    control_script_string += "  MODE_DONT_CHANGE = 2\n";
-    control_script_string += "  MODE_KEEP_LIMITS = 3\n";
-    control_script_string += "  command_speed = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]\n";
-    control_script_string += "  command_wrench = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]\n";
-    control_script_string += "  command_force_mode_limits = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]\n";
-    control_script_string += "  command_force_mode_selection_vector = [0, 0, 0, 0, 0, 0]\n";
-    control_script_string += "  command_control_mode = MODE_IDLE\n";
-    control_script_string += "  command_force_mode = MODE_RIGID\n";
-    control_script_string += "  def update_command(new_speed, new_wrench, new_fml, new_fmsv, new_control_mode, new_force_mode):\n";
-    control_script_string += "    enter_critical\n";
-    control_script_string += "    command_speed = new_speed\n";
-    control_script_string += "    command_wrench = new_wrench\n";
-    control_script_string += "    command_force_mode_limits = new_fml\n";
-    control_script_string += "    command_force_mode_selection_vector = new_fmsv\n";
-    control_script_string += "    command_control_mode = new_control_mode\n";
-    control_script_string += "    command_force_mode = new_force_mode\n";
-    control_script_string += "    exit_critical\n";
-    control_script_string += "  end\n";
-    control_script_string += "  thread command_thread():\n";
-    control_script_string += "    active_force_mode_limits = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]\n";
-    control_script_string += "    active_force_mode_selection_vector = [0, 0, 0, 0, 0, 0]\n";
-    control_script_string += "    active_control_mode = MODE_IDLE\n";
-    control_script_string += "    active_force_mode = MODE_RIGID\n";
-    control_script_string += "    while True:\n";
-    control_script_string += "      enter_critical\n";
-    control_script_string += "      cmd_speed = command_speed\n";
-    control_script_string += "      cmd_wrench = command_wrench\n";
-    control_script_string += "      cmd_force_mode_limits = command_force_mode_limits\n";
-    control_script_string += "      cmd_force_mode_selection_vector = command_force_mode_selection_vector\n";
-    control_script_string += "      cmd_control_mode = command_control_mode\n";
-    control_script_string += "      cmd_force_mode = command_force_mode\n";
-    control_script_string += "      exit_critical\n";
-    control_script_string += "      if (active_control_mode == MODE_TEACH) and (cmd_control_mode != MODE_TEACH):\n";
-    control_script_string += "        active_control_mode = MODE_IDLE\n";
-    control_script_string += "        end_teach_mode()\n";
-    control_script_string += "      elif (active_control_mode != MODE_TEACH) and (cmd_control_mode == MODE_TEACH):\n";
-    control_script_string += "        active_control_mode = MODE_TEACH\n";
-    control_script_string += "        stopj(STOP_DECELERATION)\n";
-    control_script_string += "        teach_mode()\n";
-    control_script_string += "      elif (active_control_mode != MODE_IDLE) and (cmd_control_mode == MODE_IDLE):\n";
-    control_script_string += "        active_control_mode = MODE_IDLE\n";
-    control_script_string += "        stopj(STOP_DECELERATION)\n";
-    control_script_string += "      end\n";
-    control_script_string += "      if (active_force_mode != MODE_FORCE) and (cmd_force_mode == MODE_FORCE):\n";
-    control_script_string += "        active_force_mode = MODE_FORCE\n";
-    control_script_string += "        active_force_mode_limits = cmd_force_mode_limits\n";
-    control_script_string += "        active_force_mode_selection_vector = cmd_force_mode_selection_vector\n";
-    control_script_string += "        textmsg(\"MF:\", cmd_force_mode_selection_vector)\n";
-    control_script_string += "        force_mode(get_actual_tcp_pose(), cmd_force_mode_selection_vector, cmd_wrench, 2, cmd_force_mode_limits)\n";
-    control_script_string += "      elif (active_force_mode == MODE_FORCE) and (cmd_force_mode == MODE_KEEP_LIMITS):\n";
-    //control_script_string += "        end_force_mode()\n";
-    control_script_string += "        textmsg(\"MKL:\", active_force_mode_selection_vector)\n";
-    control_script_string += "        force_mode(get_actual_tcp_pose(), active_force_mode_selection_vector, cmd_wrench, 2, active_force_mode_limits)\n";
-    control_script_string += "      elif (active_force_mode != MODE_RIGID) and (cmd_force_mode == MODE_RIGID):\n";
-    control_script_string += "        active_force_mode = MODE_RIGID\n";
-    control_script_string += "        end_force_mode()\n";
-    control_script_string += "      end\n";
-    control_script_string += "      if cmd_control_mode == MODE_SPEEDJ:\n";
-    control_script_string += "        active_control_mode = MODE_SPEEDJ\n";
-    control_script_string += "        speedj(cmd_speed, SPEED_ACCELERATION, SPEED_COMMAND_WAIT)\n";
-    control_script_string += "      elif cmd_control_mode == MODE_SPEEDL:\n";
-    control_script_string += "        active_control_mode = MODE_SPEEDL\n";
-    control_script_string += "        speedl(cmd_speed, SPEED_ACCELERATION, SPEED_COMMAND_WAIT)\n";
-    //control_script_string += "      elif cmd_control_mode == MODE_WRENCH:\n";
-    //control_script_string += "        active_control_mode = MODE_WRENCH\n";
-    //control_script_string += "        speedl(cmd_speed, SPEED_ACCELERATION, SPEED_COMMAND_WAIT)\n";
-    control_script_string += "      else:\n";
-    control_script_string += "        sync()\n";
-    control_script_string += "      end\n";
-    control_script_string += "    end\n";
-    control_script_string += "  end\n";
-    control_script_string += "  socket_open(PC_IP_ADDRESS, PC_CONTROL_PORT)\n";
-    control_script_string += "  thread_command = run command_thread()\n";
-    control_script_string += "  recv_running = 1\n";
-    control_script_string += "  while recv_running == 1:\n";
-    control_script_string += "    params = socket_read_binary_integer(6 + 6 + 6 + 6 + 3)\n";
-    control_script_string += "    valid_data = params[0]\n";
-    control_script_string += "    if valid_data > 0:\n";
-    control_script_string += "      recv_speed = [params[1] / FLOAT_CONVERSION, params[2] / FLOAT_CONVERSION, params[3] / FLOAT_CONVERSION, params[4] / FLOAT_CONVERSION, params[5] / FLOAT_CONVERSION, params[6] / FLOAT_CONVERSION]\n";
-    control_script_string += "      recv_wrench = [params[7] / FLOAT_CONVERSION, params[8] / FLOAT_CONVERSION, params[9] / FLOAT_CONVERSION, params[10] / FLOAT_CONVERSION, params[11] / FLOAT_CONVERSION, params[12] / FLOAT_CONVERSION]\n";
-    control_script_string += "      recv_force_mode_limits = [params[13] / FLOAT_CONVERSION, params[14] / FLOAT_CONVERSION, params[15] / FLOAT_CONVERSION, params[16] / FLOAT_CONVERSION, params[17] / FLOAT_CONVERSION, params[18] / FLOAT_CONVERSION]\n";
-    control_script_string += "      recv_force_mode_selection_vector = [params[19], params[20], params[21], params[22], params[23], params[24]]\n";
-    control_script_string += "      recv_control_mode = params[25]\n";
-    control_script_string += "      recv_force_mode = params[26]\n";
-    control_script_string += "      recv_running = params[27]\n";
-    control_script_string += "      if recv_running == 1:\n";
-    control_script_string += "        update_command(recv_speed, recv_wrench, recv_force_mode_limits, recv_force_mode_selection_vector, recv_control_mode, recv_force_mode)\n";
-    control_script_string += "      else:\n";
-    control_script_string += "        update_command([0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0, 0, 0, 0, 0, 0], MODE_IDLE, MODE_RIGID)\n";
-    control_script_string += "      end\n";
-    control_script_string += "    else:\n";
-    control_script_string += "      update_command([0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0, 0, 0, 0, 0, 0], MODE_IDLE, MODE_DONT_CHANGE)\n";
-    control_script_string += "    end\n";
-    control_script_string += "  end\n";
-    control_script_string += "  sleep(0.1)\n";
-    control_script_string += "  socket_close()\n";
-    control_script_string += "  kill thread_command\n";
-    control_script_string += "end\n";
+    const std::string control_script_string
+        = lightweight_ur_driver::MakeControlProgram(our_ip_address,
+                                                    control_port,
+                                                    float_conversion_ratio,
+                                                    stop_deceleration,
+                                                    speed_acceleration,
+                                                    speed_command_wait);
     const bool success = robot_ptr_->SendURScriptCommand(control_script_string);
     if (success)
     {
