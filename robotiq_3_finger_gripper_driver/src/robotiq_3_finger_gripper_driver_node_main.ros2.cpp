349ad4728d2f4bd362c7a8971c81102748c14663
Michel Hidalgo <michel@ekumenlabs.com>
2022-02-17 18:07:28 -0300

    Address peer review comments
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- robotiq_3_finger_gripper_driver/src/robotiq_3_finger_gripper_driver_node_main.ros2.cpp
+++ robotiq_3_finger_gripper_driver/src/robotiq_3_finger_gripper_driver_node_main.ros2.cpp
@@ -4,7 +4,7 @@
 
 #include "robotiq_3_finger_gripper_driver_node.ros2.hpp"
 
-int main(int argc, char * argv[])
+int main(int argc, char** argv)
 {
   auto args = rclcpp::init_and_remove_ros_arguments(argc, argv);
   using robotiq_3_finger_gripper_driver::Robotiq3FingerDriverNode;
