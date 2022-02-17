b14e5fda2c9376b09c46876e3c2ae7ea76e10675
Michel Hidalgo <michel@ekumenlabs.com>
2022-02-17 17:53:39 -0300

    Address peer review comments
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- ati_netcanoem_ft_driver/src/ati_netcanoem_ft_driver_node_main.ros2.cpp
+++ ati_netcanoem_ft_driver/src/ati_netcanoem_ft_driver_node_main.ros2.cpp
@@ -4,7 +4,7 @@
 
 #include "ati_netcanoem_ft_driver_node.ros2.hpp"
 
-int main(int argc, char * argv[])
+int main(int argc, char** argv)
 {
   auto args = rclcpp::init_and_remove_ros_arguments(argc, argv);
   using ati_netcanoem_ft_driver::AtiNetCanOemDriverNode;
